7bbf14dc4d3f8d51052939a96afdf109c3d764f2
Rafael Ribeiro <rafaelribdev@gmail.com>
2023-08-15 21:34:53 -0300

    first commit

--- index.js
+++ index.js
@@ -1,28 +1,6 @@
-const express = require('express'); // Requisitando o módulo 'express'
-const app = express(); // Qualquer coisa que eu for usar agora do express, eu vou usar a partir da variavel app
+const express = require('express');
+const app = express();
 
-// Definição de rotas
-app.get('/', function(req, res){
-    res.sendFile(__dirname + "/html/index.html");
-});
-
-app.get("/sobre", function(req, res){
-    res.sendFile(__dirname + "/html/sobre.html");
-});
-
-app.get("/blog", function(req, res){
-    res.send("Bem vindo ao meu blog!");
-});
-
-app.get('/ola/:nome/:cargo', function(req, res){
-    res.send("<h1>Olá " + req.params.nome + "</h1>" + "<h2>Seu cargo é: " + req.params.cargo + "</h2>");
-});
-
-
-// Função listen que vem do express que pede a porta em que quero rodar a minha aplicação
-// Essa função sempre tem que ser a ultima do nosso código
-// Função de callback (executada sempre que algum evento acontece)
 app.listen(8080, function(){
     console.log('Aplicação rodando na porta 8080!')
 });
-// localhost:8080
